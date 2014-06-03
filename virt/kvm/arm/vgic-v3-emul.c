df366101d32738ce5082b016a2b03165eb82b138
Andre Przywara <andre.przywara@arm.com>
2014-06-03 10:26:30 +0200

    arm/arm64: KVM: allow userland to request a virtual GICv3
    With all of the GICv3 code in place now we allow userland to ask the
    kernel for using a virtual GICv3 in the guest.
    Also we provide the necessary support for guests setting the memory
    addresses for the virtual distributor and redistributors.
    This requires some userland code to make use of that feature and
    explicitly ask for a virtual GICv3.
    
    Signed-off-by: Andre Przywara <andre.przywara@arm.com>

--- virt/kvm/arm/vgic-v3-emul.c
+++ virt/kvm/arm/vgic-v3-emul.c
@@ -1009,6 +1009,9 @@ static int vgic_v3_has_attr(struct kvm_device *dev,
 		case KVM_VGIC_V2_ADDR_TYPE_DIST:
 		case KVM_VGIC_V2_ADDR_TYPE_CPU:
 			return -ENXIO;
+		case KVM_VGIC_V3_ADDR_TYPE_DIST:
+		case KVM_VGIC_V3_ADDR_TYPE_REDIST:
+			return 0;
 		}
 		break;
 	case KVM_DEV_ARM_VGIC_GRP_DIST_REGS:
