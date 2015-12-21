dd776bd71012a2a8baca742cbb0732033f9fc536
Eric Auger <eric.auger@linaro.org>
2015-12-21 17:34:52 +0100

    KVM: arm/arm64: vgic-new: vgic_kvm_device: access to VGIC registers
    This patch implements the switches for KVM_DEV_ARM_VGIC_GRP_DIST_REGS
    and KVM_DEV_ARM_VGIC_GRP_CPU_REGS API which allows the userspace to
    access VGIC registers.
    
    At that stage the interfaces with the MMIO API are not implemented:
    - vgic_attr_regs_access
    - vgic_v2_has_attr_regs
    
    Signed-off-by: Eric Auger <eric.auger@linaro.org>

--- virt/kvm/arm/vgic/vgic.h
+++ virt/kvm/arm/vgic/vgic.h
@@ -34,6 +34,7 @@ void vgic_v2_populate_lr(struct kvm_vcpu *vcpu, struct vgic_irq *irq, int lr);
 void vgic_v2_set_underflow(struct kvm_vcpu *vcpu);
 int vgic_v2_dist_access(struct kvm_vcpu *vcpu, bool is_write,
 			int offset, int len, void *val);
+int vgic_v2_has_attr_regs(struct kvm_device *dev, struct kvm_device_attr *attr);
 
 #ifdef CONFIG_KVM_ARM_VGIC_V3
 void vgic_v3_irq_change_affinity(struct kvm *kvm, u32 intid, u64 mpidr);
